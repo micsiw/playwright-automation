54a533739acd92f6ca92e8cdf0f6cc22ddce3087
micsiw <michalsiwanowicz@gmail.com>
2023-06-29 16:55:38 +0200

    setting up environment

---
+++ playwright.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  reporter: [["list"], ["html"]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    headless: false,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL:
+      "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login",
+
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+});
